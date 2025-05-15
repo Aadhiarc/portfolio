c13cee80e38c339ea3722c3701538df7ea5ba402
Aadhiarc <aadhiarc@gmail.com>
2025-05-15 21:02:10 +0530

    splash screen

--- src/app/features/splash/splash.component.ts
+++ src/app/features/splash/splash.component.ts
@@ -13,12 +13,28 @@ export class SplashComponent implements OnInit ,AfterViewInit {
       type:'chars'
     })
      gsap.from(split.chars, {
-      x: 150,
+      x: -150,
     opacity: 0,
     duration: 0.7, 
     ease: "power4",
-    stagger: 0.04
+    stagger: {
+      each:0.05 ,
+    from: 'start',  }
     });
+
+    let percent = {value:0};
+    gsap.to(percent,{
+      value:100,
+      duration:3,
+      ease:'power4.out',
+      onUpdate:()=>{
+        let value = Math.floor(percent.value)
+        document.getElementById('loadingText')!.textContent= value+'%'
+      },
+      onComplete:()=>{
+
+      }
+    })
   }
 
   ngOnInit(): void {
